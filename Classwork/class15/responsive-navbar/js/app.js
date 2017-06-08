35ebfc8f3950c1e161cbb1b4944ba1125f6fe20e
jintyland <janetsutherland@Janets-Air.home>
2017-06-08 01:51:41 -0400

    Class 15

---
+++ Classwork/class15/responsive-navbar/js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+	// console.logs the window.width
+	$(window).resize(function () {
+		console.log($(window).width())
+
+		if($(window).width() > 640) {
+			$('#small-nav').hide()
+		}
+
+	})
+	$('#burger').click(function(){
+		$('#small-nav').slideToggle();
+	})
+
+})
